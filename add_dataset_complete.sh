91a0edb60778aca467cc4d98bf2ba1aee5a68c8d
Callum Rollo <c.rollo@outlook.com>
2022-06-17 12:08:22 +0200

    add complete datasets function

---
+++ add_dataset_complete.sh
@@ -0,0 +1,7 @@
+glider=$1
+mission=$2
+missiondir=/Data/complete_mission/SEA$glider/M$mission/timeseries
+
+sudo docker exec -it erddap-docker_erddap_1 bash -c "cd webapps/erddap/WEB-INF/ && bash GenerateDatasetsXml.sh EDDTableFromMultidimNcFiles $missiondir .\* nothing 'time, latitude, longitude, depth' default default default default default default default default default default default default default default  "
+cp /media/data/customdocker/customvolumes/erddapData/logs/GenerateDatasetsXml.out /home/ubuntu/xml_edit/original.xml
+/usr/bin/python3 /home/ubuntu/xml_edit/correct_xml.py  "$glider" "$mission" delayed
