34ae871b9358e64cd9831593652027dcbabd1f9f
Callum Rollo <c.rollo@outlook.com>
2022-06-30 13:43:08 +0200

    remove unnecessary dimension arguments

--- add_dataset_nrt.sh
+++ add_dataset_nrt.sh
@@ -2,6 +2,6 @@ glider=$1
 mission=$2
 missiondir=/Data/nrt/SEA$glider/M$mission/timeseries
 
-sudo docker exec -it erddap-docker_erddap_1 bash -c "cd webapps/erddap/WEB-INF/ && bash GenerateDatasetsXml.sh EDDTableFromMultidimNcFiles $missiondir .\* nothing 'time, latitude, longitude, depth' default default default default default default default default default default default default default default  "
+sudo docker exec -it erddap-docker_erddap_1 bash -c "cd webapps/erddap/WEB-INF/ && bash GenerateDatasetsXml.sh EDDTableFromMultidimNcFiles $missiondir .\* nothing 'time' default default default default default default default default default default default default default default  "
 cp /media/data/customdocker/customvolumes/erddapData/logs/GenerateDatasetsXml.out /home/ubuntu/xml_edit/original.xml
 /usr/bin/python3 /home/ubuntu/xml_edit/correct_xml.py  "$glider" "$mission" nrt
