6750b31b5db7d7879fffd82b30517ed0f2cdd0d0
Callum Rollo <c.rollo@outlook.com>
2022-08-15 11:01:51 +0200

    nrt datasets add usable by cron and force reload of complete chunked

--- add_dataset_chunked.sh
+++ add_dataset_chunked.sh
@@ -5,3 +5,6 @@ missiondir=/Data/complete_mission/SEA$glider/M$mission/timeseries_chunked
 sudo docker exec -it erddap-docker_erddap_1 bash -c "cd webapps/erddap/WEB-INF/ && java -cp classes:../../../lib/servlet-api.jar:lib/* -Xms8000M -Xmx8000M gov.noaa.pfel.erddap.GenerateDatasetsXml EDDTableFromMultidimNcFiles $missiondir .\* nothing 'time' default default default default default default default default default default default default default default  "
 cp /media/data/customdocker/customvolumes/erddapData/logs/GenerateDatasetsXml.out /home/ubuntu/xml_edit/original.xml
 /usr/bin/python3 /home/ubuntu/xml_edit/correct_xml.py  "$glider" "$mission" delayed
+
+flagdir=/media/data/customdocker/customvolumes/erddapData/flag/delayed_SEA0"$glider"_M"$mission"
+touch "$flagdir"
