385324beb3b15c68c47211ff4c49c097866c4967
Callum Rollo <c.rollo@outlook.com>
2022-08-16 15:02:51 +0200

    flag to reload any type of dataset after its added

--- add_dataset_nrt.sh
+++ add_dataset_nrt.sh
@@ -5,3 +5,6 @@ missiondir=/Data/nrt/SEA$glider/M$mission/timeseries
 sudo docker exec -i erddap-docker_erddap_1 bash -c "cd webapps/erddap/WEB-INF/ && bash GenerateDatasetsXml.sh EDDTableFromMultidimNcFiles $missiondir .\* nothing 'time' default default default default default default default default default default default default default default  "
 cp /media/data/customdocker/customvolumes/erddapData/logs/GenerateDatasetsXml.out /home/ubuntu/xml_edit/original.xml
 /usr/bin/python3 /home/ubuntu/xml_edit/correct_xml.py  "$glider" "$mission" nrt
+
+flagdir=/media/data/customdocker/customvolumes/erddapData/flag/delayed_SEA0"$glider"_M"$mission"
+touch "$flagdir"
