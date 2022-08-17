56f6c27acbc1aea5a8ab69df091d407b9fcdfb92
Callum Rollo <c.rollo@outlook.com>
2022-08-17 13:11:16 +0200

    correct bool check to not break on 0 index

--- times_track.py
+++ times_track.py
@@ -15,8 +15,9 @@ def update_proc_time(glider, mission, file_type):
     mtime = datetime.datetime.fromtimestamp(path.lstat().st_mtime)
     fn = f"/media/data/log/{file_type}.csv"
     df = pd.read_csv(fn, parse_dates=["proc_time", "erddap_time"])
-    a = [np.logical_and(df.glider == glider, df.mission == mission)]
-    if df.index[tuple(a)].any():
+    glider_mission_check = np.logical_and(df.glider == glider, df.mission == mission)
+    a = [glider_mission_check]
+    if glider_mission_check.any():
         ind = df.index[tuple(a)].values[0]
         df.at[ind, "proc_time"] = mtime
     else:
