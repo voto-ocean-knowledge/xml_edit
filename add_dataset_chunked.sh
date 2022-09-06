b5b1a3d518f18fe003aa93fba492189753edf711
Callum Rollo <c.rollo@outlook.com>
2022-09-06 11:56:06 +0200

    make add datasets other work from cron

--- add_dataset_chunked.sh
+++ add_dataset_chunked.sh
@@ -2,7 +2,7 @@ glider=$1
 mission=$2
 missiondir=/Data/complete_mission/SEA$glider/M$mission/timeseries_chunked
 
-sudo docker exec -it erddap-docker_erddap_1 bash -c "cd webapps/erddap/WEB-INF/ && java -cp classes:../../../lib/servlet-api.jar:lib/* -Xms8000M -Xmx8000M gov.noaa.pfel.erddap.GenerateDatasetsXml EDDTableFromMultidimNcFiles $missiondir .\* nothing 'time' default default default default default default default default default default default default default default  "
+sudo docker exec -i erddap-docker_erddap_1 bash -c "cd webapps/erddap/WEB-INF/ && java -cp classes:../../../lib/servlet-api.jar:lib/* -Xms8000M -Xmx8000M gov.noaa.pfel.erddap.GenerateDatasetsXml EDDTableFromMultidimNcFiles $missiondir .\* nothing 'time' default default default default default default default default default default default default default default  "
 cp /media/data/customdocker/customvolumes/erddapData/logs/GenerateDatasetsXml.out /home/ubuntu/xml_edit/original.xml
 /usr/bin/python3 /home/ubuntu/xml_edit/correct_xml.py  "$glider" "$mission" delayed
 
