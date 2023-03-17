4ef1acaf0d7c7c4e7163a925fe1f6392eba37c88
Callum Rollo <c.rollo@outlook.com>
2023-03-17 11:19:02 +0100

    reduce memry allocation to make datasets

--- add_dataset_complete.sh
+++ add_dataset_complete.sh
@@ -2,7 +2,7 @@ glider=$1
 mission=$2
 missiondir=/Data/complete_mission/SEA$glider/M$mission/timeseries
 xmlname=/home/ubuntu/xml_edit/xml/delayed_SEA"$glider"_M$mission"".xml
-sudo docker exec -i docker-erddap bash -c "cd webapps/erddap/WEB-INF/ && java -cp classes:../../../lib/servlet-api.jar:lib/* -Xms8000M -Xmx8000M gov.noaa.pfel.erddap.GenerateDatasetsXml EDDTableFromMultidimNcFiles $missiondir .\* nothing 'time' default default default default default default default default default default default default default default default"
+sudo docker exec -i docker-erddap bash -c "cd webapps/erddap/WEB-INF/ && java -cp classes:../../../lib/servlet-api.jar:lib/* -Xms6000M -Xmx6000M gov.noaa.pfel.erddap.GenerateDatasetsXml   EDDTableFromMultidimNcFiles $missiondir .\* nothing 'time' default default default default default default default default default default default default default default default"
 cp /media/data/erddapData/logs/GenerateDatasetsXml.out "$xmlname"
 /usr/bin/python3 /home/ubuntu/xml_edit/correct_xml.py  "$glider" "$mission" delayed
 
