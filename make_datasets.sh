0cae32c02a53500246bfbc9ff738c2070c52abe5
Callum Rollo <c.rollo@outlook.com>
2023-03-17 11:39:00 +0100

    correct add datasets script

--- make_datasets.sh
+++ make_datasets.sh
@@ -1,10 +1,7 @@
-glider=$1
-mission=$2
-missiondir=/Data/complete_mission/SEA$glider/M$mission/timeseries
-xmlname=/home/ubuntu/xml_edit/xml/delayed_SEA"$glider"_M$mission"".xml
-sudo docker exec -i docker-erddap bash -c "cd webapps/erddap/WEB-INF/ && java -cp classes:../../../lib/servlet-api.jar:lib/* -Xms8000M -Xmx8000M gov.noaa.pfel.erddap.GenerateDatasetsXml -doNotAddStandardNames  EDDTableFromMultidimNcFiles $missiondir .\* nothing 'time' default default default default default default default default default default default default default default default"
-cp /media/data/erddapData/logs/GenerateDatasetsXml.out "$xmlname"
-/usr/bin/python3 /home/ubuntu/xml_edit/correct_xml.py  "$glider" "$mission" delayed
+cd /home/ubuntu/erddapContent
+cat header.xml > new_datasets.xml
+for f in parts/nrt_*SEA*.xml; do tail -q -n +2 "$f" >> new_datasets.xml && echo "" >> new_datasets.xml; done
+for f in parts/delayed_*SEA*.xml; do tail -q -n +2 "$f" >> new_datasets.xml && echo "" >> new_datasets.xml; done
+for f in parts/adcp_*SEA*.xml; do tail -q -n +2 "$f" >> new_datasets.xml && echo "" >> new_datasets.xml; done
+echo "</erddapDatasets>"  >> new_datasets.xml
 
-flagdir=/media/data/erddapData/flag/delayed_SEA0"$glider"_M"$mission"
-touch "$flagdir"
