cd58d0121c76e5c977ac70c9bc9e37935ebdd22e
Callum Rollo <c.rollo@outlook.com>
2023-08-18 15:25:09 +0200

    add ctd data properly

--- add_ctd.sh
+++ add_ctd.sh
@@ -1,7 +1,8 @@
 missiondir=/Data/ctd
-xmlname=/home/ubuntu/erddapContent/parts/ctd_deployment.xml
+xmlname=/home/ubuntu/xml_edit/xml/ctd_deployment.xml
 docker exec -i docker-erddap bash -c "cd webapps/erddap/WEB-INF/ && bash GenerateDatasetsXml.sh EDDTableFromMultidimNcFiles $missiondir ctd_deployment.nc nothing 'time' default default default default default default default default default default default default default default default"
 cp /media/data/erddapData/logs/GenerateDatasetsXml.out "$xmlname"
 bash /home/ubuntu/xml_edit/make_datasets.sh
+/usr/bin/python3 /home/ubuntu/xml_edit/correct_ctd.py ctd_deployment
 flagdir=/media/data/erddapData/hardFlag/ctd_deployment
 touch "$flagdir"
--- make_datasets.sh
+++ make_datasets.sh
@@ -3,7 +3,6 @@ cat header.xml > new_datasets.xml
 for f in parts/nrt_*SEA*.xml; do tail -q -n +2 "$f" >> new_datasets.xml && echo "" >> new_datasets.xml; done
 for f in parts/delayed_*SEA*.xml; do tail -q -n +2 "$f" >> new_datasets.xml && echo "" >> new_datasets.xml; done
 for f in parts/adcp_*SEA*.xml; do tail -q -n +2 "$f" >> new_datasets.xml && echo "" >> new_datasets.xml; done
-echo "<dataset type="\""EDDTableFromMultidimNcFiles"\"" datasetID="\""ctd_deployment"\"" active="\""true"\"">" >> new_datasets.xml
 tail -q -n +2  parts/ctd_deployment.xml >> new_datasets.xml && echo "" >> new_datasets.xml
 echo "</erddapDatasets>"  >> new_datasets.xml
 mv new_datasets.xml datasets.xml
