f9377aedf4d5fa0bdf3dd966f04ced59d8f49fc4
Callum Rollo <c.rollo@outlook.com>
2025-04-29 12:49:55 +0200

    add ad2cp filedataset

--- make_datasets.sh
+++ make_datasets.sh
@@ -4,6 +4,7 @@ for f in parts/nrt_*.xml; do tail -q -n +2 "$f" >> new_datasets.xml && echo "" >
 for f in parts/delayed_*.xml; do tail -q -n +2 "$f" >> new_datasets.xml && echo "" >> new_datasets.xml; done
 for f in parts/meta_*.xml; do tail -q -n +2 "$f" >> new_datasets.xml && echo "" >> new_datasets.xml; done
 tail -q -n +2  parts/ctd_deployment.xml >> new_datasets.xml && echo "" >> new_datasets.xml
+tail -q -n +2  parts/ad2cp.xml >> new_datasets.xml && echo "" >> new_datasets.xml
 cat  parts/requests.xml >> new_datasets.xml && echo "" >> new_datasets.xml
 cat  parts/locations.xml >> new_datasets.xml && echo "" >> new_datasets.xml
 cat  parts/SB2120_M3_delayed.xml >> new_datasets.xml && echo "" >> new_datasets.xml
