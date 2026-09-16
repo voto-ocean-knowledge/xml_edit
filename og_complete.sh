01e25000bef535e918ea55712595d873da6b2146
Callum Rollo <c.rollo@outlook.com>
2026-09-16 11:38:21 +0200

    more mem

--- og_complete.sh
+++ og_complete.sh
@@ -2,7 +2,7 @@ glider=$1
 mission=$2
 missiondir=/Data/OG_complete/$glider/M$mission/timeseries
 xmlname=/home/usrerddap/erddap/xml_edit/xml/OG_complete_"$glider"_M$mission"".xml
-docker exec -i docker-erddap bash -c "cd webapps/erddap/WEB-INF/ && bash GenerateDatasetsXml.sh EDDTableFromMultidimNcFiles $missiondir .\* nothing default default default default default default default default default default default default default default default default"
+docker exec -i docker-erddap bash -c "cd webapps/erddap/WEB-INF/ && java -cp classes:../../../lib/servlet-api.jar:lib/* -Xms6000M -Xmx6000M gov.noaa.pfel.erddap.GenerateDatasetsXml EDDTableFromMultidimNcFiles $missiondir .\* nothing default default default default default default default default default default default default default default default default"
 cp /data/erddapData/logs/GenerateDatasetsXml.out "$xmlname"
 /usr/bin/python3 /home/usrerddap/erddap/xml_edit/og1_xml.py  "$glider" "$mission"
 
